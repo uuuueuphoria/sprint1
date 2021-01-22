d53b8e15e24df0ba3d9c6bc2ba490187eb68cd33
uuuueuphoria <47934058+uuuueuphoria@users.noreply.github.com>
2021-01-22 15:00:28 -0700

    not working

---
+++ my-app/src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App,showDescription,showMyPicture} from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <showMyPicture url="img/Datou.jpeg"/>,
+  document.querySelector('.cat')
+)
+
+ReactDOM.render(
+  <showDescription text="This is my cat"/>,
+  document.querySelector('.cat')
+)
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
